bee629461234976720f1afef0a2a3e9935f910f7
Jasper Ginn <jasperginn@gmail.com>
2025-05-11 09:53:40 +0200

    dchore: update spacing

--- tests/cli/test_cli_utils.py
+++ tests/cli/test_cli_utils.py
@@ -6,15 +6,15 @@ from rfcrew.cli import Common
 
 
 def test_common_model_passes(tmp_path: plb.Path):
-	"""Test the Common model."""
-	# Create an instance of the Common model with test data
-	Common(verbose=True, output_directory=tmp_path, otlp_endpoint=None)
+    """Test the Common model."""
+    # Create an instance of the Common model with test data
+    Common(verbose=True, output_directory=tmp_path, otlp_endpoint=None)
 
 
 def test_common_model_with_bad_oltp(tmp_path: plb.Path):
-	"""Test the Common model with a bad otlp endpoint."""
-	# Create an instance of the Common model with test data
-	with pytest.raises(ConnectionError):
-		Common(
-			verbose=True, output_directory=tmp_path, otlp_endpoint='http://bad-otel-collector:4318'
-		)
+    """Test the Common model with a bad otlp endpoint."""
+    # Create an instance of the Common model with test data
+    with pytest.raises(ConnectionError):
+        Common(
+            verbose=True, output_directory=tmp_path, otlp_endpoint='http://bad-otel-collector:4318'
+        )
--- tests/integration/test_score_agent.py
+++ tests/integration/test_score_agent.py
@@ -5,15 +5,15 @@ from rfcrew.crews.assessor import ScoreAgent
 
 @pytest.fixture
 def score_agent() -> ScoreAgent:
-	"""Provides a ScoreAgent instance for testing."""
-	return ScoreAgent(model='gemini/gemini-2.5-flash-preview-04-17')
+    """Provides a ScoreAgent instance for testing."""
+    return ScoreAgent(model='gemini/gemini-2.5-flash-preview-04-17')
 
 
 @pytest.mark.llm
 def test_score_agent_execute(score_agent: ScoreAgent):
-	"""Test the execute method of ScoreAgent."""
-	# Mock input data
-	input_data = {'notes': 'This is a test note.'}
+    """Test the execute method of ScoreAgent."""
+    # Mock input data
+    input_data = {'notes': 'This is a test note.'}
 
-	# Execute the agent
-	score_agent.execute(input_data)
+    # Execute the agent
+    score_agent.execute(input_data)
